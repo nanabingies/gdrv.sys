ba2ac79fb91a344e40a984316b826b268cc22f77
Emmanuel Sasu <nanabingies1@gmail.com>
2022-08-26 12:24:09 +0000

    Update sub_12D10.c

--- Reversed Codes/sub_12D10.c
+++ Reversed Codes/sub_12D10.c
@@ -1,5 +1,16 @@
 
 NTSTATUS sub_12D10(_In_ PDEVICE_OBJECT DeviceObject, _In_ PIRP Irp){
 
-    
+    PIO_STACK_LOCATION stack = IoGetCurrentIrpStackLocation(Irp);
+    Irp->IoStatus.Information = 0;
+    PVOID ext = &DeviceObject->DeviceExtension;
+    PVOID buffer = Irp->AssociatedIrp.SystemBuffer;
+    UCHAR MajorFunction = stack->MajortFunction;
+    ULONG InputBufferLength = stack->DeviceIoControl.InputBufferLength;
+    ULONG OutputBufferLength = stack->DeviceIoControl.OutputBufferLength;
+    NTSTATUS status = STATUS_NOT_IMPLEMENTED;
+
+    if (MajorFunction == 0x0 || MajorFunction == 0x2){
+
+    }
 }
