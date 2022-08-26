2e1088e98fe89be3c51ccefb45c4f4b62fd3c6da
Emmanuel Sasu <nanabingies1@gmail.com>
2022-08-26 16:01:46 +0000

    Update sub_12D10.c

--- Reversed Codes/sub_12D10.c
+++ Reversed Codes/sub_12D10.c
@@ -61,12 +61,91 @@ NTSTATUS sub_12D10(_In_ PDEVICE_OBJECT DeviceObject, _In_ PIRP Irp){
                 status = STATUS_INVALID_PARAMETER;
                 goto Exit;
             }
+
             DbgPrint("Sizeof(PVOID)=%d", sizeof(PVOID));
             DbgPrint("MAPMEM.SYS: memory successfully mapped");
             Irp->IoStatus.Information = 0x8;
         }
 
         case 0xC3502008:
+        {
+            DbgPrint("InputBufferLength=%d,szieof(PVOID)=%d", InputBufferLength, sizeof(PVOID));
+            if (InputBufferLength < 8){
+                DbgPrint("MAPMEM.SYS: ZwUnmapViewOfSection failed");
+                status = STATUS_INSUFFICIENT_RESOURCES;
+                goto Exit;
+            }
+
+            ZwUnmapViewOfSection(PsGetCurrentProcess(), buffer);
+            DbgPrint("MAPMEM.SYS: memory successfully unmapped");
+        }
+
+        case 0xC3502580:
+        {
+            status = GIO_MSR(ext, Irp, stack, IoControlCode);   // recheck params
+        }
+
+        case 0xC3500E68:
+        {
+            status = sub_11C70(DeviceObject, Irp, stack);       // recheck params
+        }
+
+        case 0xC3502804:
+        {
+            status = sub_127C0(ext, Irp, stack, IoControlCode);          // recheck params
+        }
+
+        case 0xC3502808:
+        {
+            status = GIO_memcpy(ext, Irp, stack, IoControlCode);         // recheck params
+        }
+
+        case 0xC350280C:
+        {
+            status = sub_128F0(ext, Irp, stack, IoControlCode);
+        }
+
+        case 0xC3506400:
+        {
+            status = sub_11270(Irp, stack);                             // recheck params
+        }
+
+        case 0xC3506404:
+        {
+            status = sub_11270(Irp, stack);                             // recheck params
+        }
+
+        case 0xC3506408:
+        {
+            status = sub_11270(Irp, stack);                             // recheck params
+        }
+
+        case 0xC350A440:
+        {
+            status = sub_11380(Irp, stack);                             // recheck params
+        }
+
+        case 0xC350A444:
+        {
+            status = sub_11380(Irp, stack);                             // recheck params
+        }
+
+        case 0xC350A448:
+        {
+            status = sub_11380(Irp, stack);                             // recheck params
+        }
+
+        case 0xC350E44C:
+        {
+            status = sub_115F0(Irp, stack);
+        }
+
+        case 0xC350E454:
+        {
+            status = sub_11A00(DeviceObject);
+        }
+
+        case 0xC3502844:
         {
             
         }
