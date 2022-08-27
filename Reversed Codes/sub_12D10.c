7467eba62bac972539a3f37c687e74ccee97afe8
Emmanuel Sasu <nanabingies1@gmail.com>
2022-08-27 11:39:02 +0000

    Update sub_12D10.c

--- Reversed Codes/sub_12D10.c
+++ Reversed Codes/sub_12D10.c
@@ -1,4 +1,17 @@
 
+struct _v2{
+        PVOID Unknown1;     // 0x00
+        PMDL mdl;           // 0x08
+        PVOID Unknown2;     // 0x10
+        PVOID BaseAddress;  // 0x18
+        SIZE_T Size;        // 0x20
+};
+
+struct _v3{
+    ULONG Unknown1;         // 0x00
+    ULONG Unknown2;         // 0x04
+};
+
 NTSTATUS sub_12D10(_In_ PDEVICE_OBJECT DeviceObject, _In_ PIRP Irp){
 
     PIO_STACK_LOCATION stack = IoGetCurrentIrpStackLocation(Irp);
@@ -29,16 +42,19 @@ NTSTATUS sub_12D10(_In_ PDEVICE_OBJECT DeviceObject, _In_ PIRP Irp){
             DbgPrint("In Alloc function!!");
             status = sub_12710(ext, Irp, stack, IoControlCode);
             DbgPrint("Exit Alloc function!!");
+            break;
         }
 
         case 0xC3500E68:
         {
-            status = sub_11C70(DeviceObject, Irp, stack);  // recheck params
+            status = sub_11C70(DeviceObject, Irp, stack);                   // recheck params
+            break;
         }
 
         case 0xC3500E6C:
         {
-            status = sub_11AF0(DeviceObject, Irp, stack);  // recheck params
+            status = sub_11AF0(DeviceObject, Irp, stack);                   // recheck params
+            break;
         }
 
         case 0xC3500E70:
@@ -46,11 +62,13 @@ NTSTATUS sub_12D10(_In_ PDEVICE_OBJECT DeviceObject, _In_ PIRP Irp){
             DbgPrint("In SMBUS Control");
             status = sub_12C00(DeviceObject, Irp, stack);
             DbgPrint("Exit SMBUS Control");
+            break;
         }
 
         case 0xC3502000:
         {
-            status = sub_11570(ext, Irp, stack, IoControlCode); // recheck params
+            status = sub_11570(ext, Irp, stack, IoControlCode);             // recheck params
+            break;
         }
 
         case 0xC3502004:
@@ -65,6 +83,7 @@ NTSTATUS sub_12D10(_In_ PDEVICE_OBJECT DeviceObject, _In_ PIRP Irp){
             DbgPrint("Sizeof(PVOID)=%d", sizeof(PVOID));
             DbgPrint("MAPMEM.SYS: memory successfully mapped");
             Irp->IoStatus.Information = 0x8;
+            break;
         }
 
         case 0xC3502008:
@@ -78,76 +97,138 @@ NTSTATUS sub_12D10(_In_ PDEVICE_OBJECT DeviceObject, _In_ PIRP Irp){
 
             ZwUnmapViewOfSection(PsGetCurrentProcess(), buffer);
             DbgPrint("MAPMEM.SYS: memory successfully unmapped");
+            break;
         }
 
         case 0xC3502580:
         {
             status = GIO_MSR(ext, Irp, stack, IoControlCode);   // recheck params
+            break;
         }
 
         case 0xC3500E68:
         {
-            status = sub_11C70(DeviceObject, Irp, stack);       // recheck params
+            status = sub_11C70(DeviceObject, Irp, stack);               // recheck params
+            break;
         }
 
         case 0xC3502804:
         {
             status = sub_127C0(ext, Irp, stack, IoControlCode);          // recheck params
+            break;
         }
 
         case 0xC3502808:
         {
             status = GIO_memcpy(ext, Irp, stack, IoControlCode);         // recheck params
+            break;
         }
 
         case 0xC350280C:
         {
             status = sub_128F0(ext, Irp, stack, IoControlCode);
+            break;
         }
 
         case 0xC3506400:
         {
             status = sub_11270(Irp, stack);                             // recheck params
+            break;
         }
 
         case 0xC3506404:
         {
             status = sub_11270(Irp, stack);                             // recheck params
+            break;
         }
 
         case 0xC3506408:
         {
             status = sub_11270(Irp, stack);                             // recheck params
+            break;
         }
 
         case 0xC350A440:
         {
             status = sub_11380(Irp, stack);                             // recheck params
+            break;
         }
 
         case 0xC350A444:
         {
             status = sub_11380(Irp, stack);                             // recheck params
+            break;
         }
 
         case 0xC350A448:
         {
             status = sub_11380(Irp, stack);                             // recheck params
+            break;
         }
 
         case 0xC350E44C:
         {
             status = sub_115F0(Irp, stack);
+            break;
         }
 
         case 0xC350E454:
         {
             status = sub_11A00(DeviceObject);
+            break;
         }
 
         case 0xC3502844:
         {
-            
+            if (InputBufferLength != 0x10){
+                status = STATUS_INVALID_PARAMETER;
+                goto Exit;
+            }
+
+            *(DWORD*)(buffer + 0x10) = 0;
+            struct _v2* v2 = *(PVOID*)(ext + 0x18);
+            if (!v2){
+                goto Exit;
+            }
+
+            if (buffer == v2->BaseAddress){
+                if (*(DWORD*)buffer + 0x8 == v2->Size){
+                    MmUnmapLockedPages(v2->BaseAddress, v2->mdl);
+                    IoFreeMdl(v2->mdl);
+                    MmUnmapIoSpace(v2->Unknown2, v2->Size);
+                    if (v2->Unknown1 == *(PVOID*)(buffer + 0x18)){
+                        ExFreePoolWithTag(v2, 0x0);
+                        v2 = v2->Unknown1;
+                        status = STATUS_SUCCESS;
+                        goto Exit;
+                    }
+
+                    ExFreePoolWithTag(v2, 0x0);
+                    DeviceObject = v2;
+                    goto Exit;
+                }
+                else{
+                    status = STATUS_INVALID_PARAMETER;
+                    goto Exit;
+                }
+            }
+            break;
+        }
+
+        case 0xC3502840:
+        {
+            if (InputBufferLength == 0x10){
+                struct _v3 v3 = buffer;
+                if (OutputBufferLength == InputBufferLength){
+                    PVOID IoSpace = MmMapIoSpace(v3->Unknown1, DeviceObject, NULL);
+                    if (!IoSpace){
+                        status = STATUS_INSUFFICIENT_RESOURCES;
+                        goto Exit;
+                    }
+
+                    PMDL mdl = IoAllocateMdl
+                }
+            }
         }
     }
 
